ced7eed9b24f394ca9f434ecafd2c1b5704b6d4d
Gian Carlo Val Ebao <gianebao@gmail.com>
2017-07-13 11:37:38 +0800

    enabled offline access using service workers

---
+++ manifest.js
@@ -0,0 +1,28 @@
+{
+  "short_name": "MatchFarm",
+  "name": "Matchland Farming Data Tool",
+  "display": "standalone",
+  "start_url": "index.htm",
+  "icons": [
+    {
+      "src": "icon/icon-57x57.jpg",
+      "type": "image/jpg",
+      "size": "57x57"
+    },
+    {
+      "src": "icon/icon-72x72.jpg",
+      "type": "image/jpg",
+      "size": "72x72"
+    },
+    {
+      "src": "icon/icon-114x114.jpg",
+      "type": "image/jpg",
+      "size": "114x114"
+    },
+    {
+      "src": "icon/icon-114x114.jpg",
+      "type": "image/jpg",
+      "size": "114x114"
+    }
+  ]
+}
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+var version = '20170712';
+var cacheName = 'test-cache-2' + version;
+var filesToCache = [
+  'dataset.'+version+'.js',
+
+  'index.htm',
+  'manifest.js',
+  'service-worker.js',
+
+  'asset/bootstrap.min.css',
+  'asset/jquery.dataTables.min.css',
+  'asset/jquery-1.12.4.min.js',
+  'asset/dataTables.bootstrap.min.js',
+  'asset/jquery.dataTables.min.js',
+
+  'icon/favicon.png',
+  'icon/icon-57x57.jpg',
+  'icon/icon-72x72.jpg',
+  'icon/icon-114x114.jpg',
+  'icon/icon-144x144.jpg',
+
+  'images/sort_both.png',
+  'images/sort_asc.png',
+  'images/sort_desc.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
